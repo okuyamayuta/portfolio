7a2da80dd3ea75a3a92cb4b0693735b96f6e7dc9
okkun0115 <kamakirion@gmail.com>
2025-07-13 15:38:39 +0900

    Delete springboot directory

--- springboot/samuraitravel/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
- const stripe = Stripe('pk_test_51OwHic2La9ZPRkrZxI5IUIERdGRAKV2IMt8pIiO5Zbriz8Ny6kXuhdATQY7ZhugMWspcx9oZWhzRP6kmRTX2irAj00Ed4ydc4X');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
